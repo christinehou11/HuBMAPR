684c0826d7a68a1a64e68555960deff390b5cad6
Federico Marini <marinif@uni-mainz.de>
2022-11-29 11:20:45 +0100

    doc'ing and exporting `dataset()`

--- R/dataset.R
+++ R/dataset.R
@@ -4,6 +4,14 @@ DATASET <- paste0(BASE_URL, "/browse/dataset")
 #'
 #' @title Retrieve information about a single dataset
 #'
+#' @param uuid Character string, corresponding to the HuBMAP UUID string. This is
+#' expected to be a 32-digit hex number.
+#'
+#' @details Additional details are provided on the HuBMAP consortium webpage,
+#' https://software.docs.hubmapconsortium.org/apis
+#'
+#' @export
+#'
 #' @examples
 #' uuid <-
 #'     datasets() |>
@@ -14,8 +22,10 @@ DATASET <- paste0(BASE_URL, "/browse/dataset")
 dataset <-
     function(uuid)
 {
+    stopifnot(nchar(uuid) == 32L)
     uri <- paste0(DATASET, "/", uuid, ".json")
     response <- httr::GET(uri)
     httr::stop_for_status(response)
     json <- httr::content(response, as = "text", encoding = "UTF-8")
+    return(json)
 }
