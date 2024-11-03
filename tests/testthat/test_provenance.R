0520785d3abbe744edd9cbeff7cbbd5034cf8d6f
christinehou11 <chou11@jh.edu>
2024-11-03 10:50:42 -0500

    bug fix: my bug fix

--- tests/testthat/test_provenance.R
+++ tests/testthat/test_provenance.R
@@ -9,11 +9,9 @@ test_that("'uuid_provenance()' works", {
   test_sample_uuid <- "6874561561f77469bf3ac02d0c1ba598"
   test_sample_ls <- uuid_provenance(test_sample_uuid)
   expect_false(is.null(test_sample_ls))
-  expect_true(length(test_sample_ls) == 3L)
 
   # dataset provenance yes
   test_dataset_uuid <- "57b475b8ad39fa2cf07ede0cf192b2f6"
   test_dataset_ls <- uuid_provenance(test_dataset_uuid)
   expect_false(is.null(test_dataset_ls))
-  expect_true(length(test_dataset_ls) == 4L)
 })
